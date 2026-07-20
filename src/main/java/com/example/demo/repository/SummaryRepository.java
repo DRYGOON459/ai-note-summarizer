cffbf1d8eafb915dbce6c408bac84cdd944d6745
DRYGOON459 <visheshj403@gmail.com>
2026-07-20 22:31:13 +0530

    Service

--- src/main/java/com/example/demo/repository/SummaryRepository.java
+++ src/main/java/com/example/demo/repository/SummaryRepository.java
@@ -1,8 +1,10 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.SavedSummary;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.demo.model.SavedSummary;
+
 public interface SummaryRepository extends JpaRepository<SavedSummary, Long> {
+
 
 }
