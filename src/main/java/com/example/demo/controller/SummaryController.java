dff261ba875e0eecaa9a044ccf3bd50d78f6b5bb
DRYGOON459 <visheshj403@gmail.com>
2026-07-27 23:35:22 +0530

    updated summary controller Please enter the commit message for your changes. Lines starting

--- src/main/java/com/example/demo/controller/SummaryController.java
+++ src/main/java/com/example/demo/controller/SummaryController.java
@@ -25,7 +25,6 @@ public class SummaryController {
 
     public SummaryController(SummaryService summaryService) {
         this.summaryService = summaryService;
-        
     }
 
     @PostMapping("/summarize")
